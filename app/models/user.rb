8febebb58c6c21c721076651c98baa9be9610755
MartinCantarini <MartinCantarini@users.noreply.github.com>
2016-10-06 14:11:34 -0300

    06/10/2016-login con facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :nombre, :apellido, :apodo, :hincha,:localidad,:provincia,:pais,:sexo,:telefono,:foto,presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable,
          :omniauthable, :omniauth_providers => [:facebook]
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'home/index'
   
   devise_for :users, :controllers => { registrations: 'registrations', :omniauth_callbacks => "users/omniauth_callbacks"}
-resources :users
+  resources :users
+  get 'user/update' => "users#update"
+  get 'user/edit'    => "users#edit"
+  post 'user/update' => "users#update"
+  post 'user/edit'    => "users#edit"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
